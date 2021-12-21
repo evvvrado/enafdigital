4e286aa2e37c520b328932b4e4d395840c2f45e9
evvvrado <everaldocrj@gmail.com>
2021-12-21 16:50:34 -0300

    update

--- resources/views/site/feira-empresas.blade.php
+++ resources/views/site/feira-empresas.blade.php
@@ -6,103 +6,99 @@ $categoriaid = $expositores->first();
 @endphp
 
 @if ($categoriaid)
-    <title>ENAF - {{ $categorias[$categoriaid->categoria] }}</title>
+<title>ENAF - {{ $categorias[$categoriaid->categoria] }}</title>
 @else
-    <title>ENAF- Vazio</title>
+<title>ENAF- Vazio</title>
 @endif
 
 <body id="feiraEmpresas">
 
-    @include("site.includes.navbar")
+  @include("site.includes.navbar")
 
-    <section class="container-fluid s_hero">
-        <div class="container-fav">
-            <div class="_h1 fade">
-                <img src="{{ asset('site/img/svg_poweroflife_minor.svg') }}" alt="Simbolo Power of Life" />
-            </div>
+  <section class="container-fluid s_hero">
+    <div class="container-fav">
+      <div class="_h1 fade">
+        <img src="{{ asset('site/img/svg_poweroflife_minor.svg') }}" alt="Simbolo Power of Life" />
+      </div>
+    </div>
+  </section>
+
+  <section class="container-fluid s_backtoFeira">
+    <div class="container-fav">
+      <div style="padding: 15px; display: flex; gap: 1rem; align-items:center;">
+        <div class="_icon">
+          <img src="{{ asset('site/img/halfarrowleft_back.svg') }}" alt="Seta para esquerda" />
         </div>
-    </section>
+        <a href="{{ route('site.feira') }}">Voltar para feira</a>
+      </div>
+    </div>
+  </section>
 
-    <section class="container-fluid s_backtoFeira">
-        <div class="container-fav">
-            <div style="padding: 15px; display: flex; gap: 1rem; align-items:center;">
-                <div class="_icon">
-                    <img src="{{ asset('site/img/halfarrowleft_back.svg') }}" alt="Seta para esquerda" />
-                </div>
-                <a href="{{ route('site.feira') }}">Voltar para feira</a>
+  <section class="container-fluid s_empresas">
+    <div class="container-fav">
+      @if ($anunciante)
+      <div class="_box main">
+        <a href=" @if ($anunciante->expositor->hotsite &&
+                        $anunciante->expositor->hotsite->slug){{ route('site.hotsite', ['slug' => $anunciante->expositor->hotsite->slug]) }}" @else{{ $anunciante->expositor->site }}
+          @endif">
+          <picture>
+            <source media="(max-width: 660px)" srcset="{{ asset($anunciante->imagem_mobile) }}">
+            <img src="{{ asset($anunciante->imagem_desktop) }}" alt="Empresa Nike" />
+          </picture>
+        </a>
+
+        <div>
+          <span>{{ $anunciante->nome }}</span>
+
+          <span>
+            <div class="_icon">
+              <img src="{{ asset('site/img/icon_cursor.svg') }}" alt="Ícone de Cursor" />
             </div>
+
+            <a href="{{ $anunciante->expositor->site }}" target="_blank">{{ $anunciante->expositor->site }}</a>
+          </span>
+
+          <span>
+            <div class="_icon">
+              <img src="{{ asset('site/img/icon_call.svg') }}" alt="Ícone de Chamada" />
+            </div>
+
+            <a href="tel:{{ \App\Classes\Util::limparString($anunciante->expositor->telefone) }}">{{ $anunciante->expositor->telefone }}</a>
+          </span>
+
+          <span>
+            <div class="_icon">
+              <img src="{{ asset('site/img/icon_envelop.svg') }}" alt="Ícone de Evenelope" />
+            </div>
+
+            <a href="mail:{{ $anunciante->expositor->email }}">{{ $anunciante->expositor->email }}</a>
+          </span>
         </div>
-    </section>
+      </div>
+      @endif
 
-    <section class="container-fluid s_empresas">
-        <div class="container-fav">
-            @if ($anunciante)
-                <div class="_box main">
-                    <a href=" @if ($anunciante->expositor->hotsite &&
-                        $anunciante->expositor->hotsite->slug){{ route('site.hotsite', ['slug' => $anunciante->expositor->hotsite->slug]) }}"
-                        @else{{ $anunciante->expositor->site }}
-                        @endif">
-                        <picture>
-                            <source media="(max-width: 660px)" srcset="{{ asset($anunciante->imagem_mobile) }}">
-                            <img src="{{ asset($anunciante->imagem_desktop) }}" alt="Empresa Nike" />
-                        </picture>
-                    </a>
+      @foreach ($expositores->where("destaque", true) as $expositor)
+      <div class="_box">
+        <picture>
+          <a href=" @if ($expositor->hotsite &&
+                            $expositor->hotsite->slug){{ route('site.hotsite', ['slug' => $expositor->hotsite->slug]) }}" @else{{ $expositor->site }}
+            @endif">
+            <img src="{{ asset($expositor->foto) }}" alt="{{ $expositor->nome }}" />
+          </a>
+        </picture>
 
-                    <div>
-                        <span>{{ $anunciante->nome }}</span>
+        <div>
+          <span>{{ $expositor->nome }}</span>
+          <ul>
+            <li><a href="{{ $expositor->site }}">{{ $expositor->site }}</a></li>
+            <li><a href="tel:{{ $expositor->telefone }}">{{ $expositor->telefone }}</a></li>
+            <li><a href="mailto:{{ $expositor->email }}">{{ $expositor->email }}</a></li>
+          </ul>
+        </div>
+      </div>
+      @endforeach
 
-                        <span>
-                            <div class="_icon">
-                                <img src="{{ asset('site/img/icon_cursor.svg') }}" alt="Ícone de Cursor" />
-                            </div>
-
-                            <a href="{{ $anunciante->expositor->site }}"
-                                target="_blank">{{ $anunciante->expositor->site }}</a>
-                        </span>
-
-                        <span>
-                            <div class="_icon">
-                                <img src="{{ asset('site/img/icon_call.svg') }}" alt="Ícone de Chamada" />
-                            </div>
-
-                            <a
-                                href="tel:{{ \App\Classes\Util::limparString($anunciante->expositor->telefone) }}">{{ $anunciante->expositor->telefone }}</a>
-                        </span>
-
-                        <span>
-                            <div class="_icon">
-                                <img src="{{ asset('site/img/icon_envelop.svg') }}" alt="Ícone de Evenelope" />
-                            </div>
-
-                            <a href="mail:{{ $anunciante->expositor->email }}">{{ $anunciante->expositor->email }}</a>
-                        </span>
-                    </div>
-                </div>
-            @endif
-
-            @foreach ($expositores->where("destaque", true) as $expositor)
-                <div class="_box">
-                    <picture>
-                        <a href=" @if ($expositor->hotsite &&
-                            $expositor->hotsite->slug){{ route('site.hotsite', ['slug' => $expositor->hotsite->slug]) }}"
-                            @else{{ $expositor->site }}
-                            @endif">
-                            <img src="{{ asset($expositor->foto) }}" alt="{{ $expositor->nome }}" />
-                        </a>
-                    </picture>
-
-                    <div>
-                        <span>{{ $expositor->nome }}</span>
-                        <ul>
-                            <li><a href="{{ $expositor->site }}">{{ $expositor->site }}</a></li>
-                            <li><a href="tel:{{ $expositor->telefone }}">{{ $expositor->telefone }}</a></li>
-                            <li><a href="mailto:{{ $expositor->email }}">{{ $expositor->email }}</a></li>
-                        </ul>
-                    </div>
-                </div>
-            @endforeach
-
-            {{-- <div class="_box">
+      {{-- <div class="_box">
         <picture>
           <a href="{{ route('site.hotsite')}}">
             <img src="{{ asset('site/img/logo_empresa (3') }}).png" alt="Empresa Embaixadora" />
@@ -287,51 +283,50 @@ $categoriaid = $expositores->first();
           </ul>
         </div>
       </div> --}}
-        </div>
-    </section>
+    </div>
+  </section>
 
-    <section class="container-fluid s_outrasEmpresas">
-        <div class="container-fav">
-            <div class="_title">
-                <h4>Outras empresas</h4>
-            </div>
+  <section class="container-fluid s_outrasEmpresas">
+    <div class="container-fav">
+      <div class="_title">
+        <h4>Outras empresas</h4>
+      </div>
 
-            <ul>
+      <ul>
 
-                @foreach ($expositores->where("destaque", false) as $expositor)
+        @foreach ($expositores->where("destaque", false) as $expositor)
 
-                    <li>
-                        <a href="{{ $expositor->site }}">
-                            <span>{{ $expositor->nome }}</span>
-
-
-                            <main>
-                                <legend>
-                                    <div class="_icon">
-                                        <img src="http://enafdigital.test/site/img/icon_call.svg"
-                                            alt="Ícone de Chamada">
-                                    </div>
-                                    {{ $expositor->telefone }}
-                                </legend>
-
-                                <legend>
-                                    <div class="_icon">
-                                        <img src="http://enafdigital.test/site/img/icon_call.svg"
-                                            alt="Ícone de Chamada">
-                                    </div>
-                                    {{ $expositor->telefone }}
-                                </legend>
-
-                                <legend>{{ $expositor->site }}</legend>
-                            </main>
-                        </a>
-                    </li>
-
-                @endforeach
+        <li>
+          <a href="{{ $expositor->site }}">
+            <span>{{ $expositor->nome }}</span>
 
 
+            <main>
+              {{--
+              <legend>
+                <div class="_icon">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
+                </div>
+                {{ $expositor->telefone }}
+              </legend> --}}
 
-                {{-- <li>
+              <legend>
+                <div class="_icon">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
+                </div>
+                {{ $expositor->telefone }}
+              </legend>
+
+              <legend>{{ $expositor->site }}</legend>
+            </main>
+          </a>
+        </li>
+
+        @endforeach
+
+
+
+        {{-- <li>
           <a href="https://probiótica.com.br">
             <span>Probiótica</span>
 
@@ -339,14 +334,14 @@ $categoriaid = $expositores->first();
             <main>
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
 
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
@@ -364,14 +359,14 @@ $categoriaid = $expositores->first();
             <main>
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
 
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
@@ -389,14 +384,14 @@ $categoriaid = $expositores->first();
             <main>
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
 
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
@@ -414,14 +409,14 @@ $categoriaid = $expositores->first();
             <main>
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
 
               <legend>
                 <div class="_icon">
-                  <img src="http://enafdigital.test/site/img/icon_call.svg" alt="Ícone de Chamada">
+                  <img src="{{ asset('/site/img/icon_call.svg') }}" alt="Ícone de Chamada">
                 </div>
                 35 9 8809 4603
               </legend>
@@ -430,15 +425,15 @@ $categoriaid = $expositores->first();
             </main>
           </a>
         </li> --}}
-            </ul>
-        </div>
-    </section>
+      </ul>
+    </div>
+  </section>
 
-    @include('site.includes.publicidade')
-
-
-
-    @include("site.includes.parceiros")
+  @include('site.includes.publicidade')
 
 
-    @include("site.includes.footer-expanded")
+
+  @include("site.includes.parceiros")
+
+
+  @include("site.includes.footer-expanded")
