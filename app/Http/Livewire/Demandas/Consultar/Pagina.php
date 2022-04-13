6e0402239507d2e1665b6f34c18f5264d18c6f28
evvvrado <everaldocrj@gmail.com>
2022-04-13 16:42:45 -0300

    SAVEPOINT

--- app/Http/Livewire/Demandas/Consultar/Pagina.php
+++ app/Http/Livewire/Demandas/Consultar/Pagina.php
@@ -13,7 +13,7 @@ class Pagina extends Component
     public $descricao;
     public $solicitante_id;
     public $solicitado_id;
-    public $finalizada;
+    public $finalizada = 0;
     public $estimativa;
     public $titulo;
     public $urgente;
@@ -22,68 +22,75 @@ class Pagina extends Component
 
     protected $listeners = ["atualizaConsulta" => '$refresh'];
 
-    public function mount($usuarios){
+    public function mount($usuarios)
+    {
         $this->usuarios = $usuarios;
-        $this->setor = 0;
+        $this->setor = 2;
     }
 
-    public function editar($demenda){
+    public function editar($demenda)
+    {
         $this->emit("carregaModalEdicao", ["demanda" => $demenda]);
     }
 
-    public function excluir($demanda){
+    public function excluir($demanda)
+    {
         $demanda = Demanda::find($demanda);
         Storage::delete($demanda->arquivo);
         $demanda->delete();
     }
 
-    public function finalizar($demanda){
+    public function finalizar($demanda)
+    {
         $demanda = Demanda::find($demanda);
         $demanda->finalizada = true;
         $demanda->entrega = date("Y-m-d");
         $demanda->save();
     }
 
-    public function trocaSetor($setor){
+    public function trocaSetor($setor)
+    {
         $this->setor = $setor;
         $this->limpaFiltros();
     }
 
-    public function setFiltros(){
+    public function setFiltros()
+    {
         $this->filtros = [];
 
-        if($this->titulo){
+        if ($this->titulo) {
             $this->filtros[] = ["titulo", "LIKE", "%" . $this->titulo . "%"];
         }
-        
-        if($this->descricao){
+
+        if ($this->descricao) {
             $this->filtros[] = ["descricao", "LIKE", "%" . $this->descricao . "%"];
         }
 
-        if($this->solicitante_id !== null && $this->solicitante_id != -1){
+        if ($this->solicitante_id !== null && $this->solicitante_id != -1) {
             $this->filtros[] = ["solicitante_id", "=", $this->solicitante_id];
         }
 
-        if($this->solicitado_id !== null && $this->solicitado_id != -1){
+        if ($this->solicitado_id !== null && $this->solicitado_id != -1) {
             $this->filtros[] = ["solicitado_id", "=", $this->solicitado_id];
         }
 
-        if($this->urgente !== null && $this->urgente != -1){
+        if ($this->urgente !== null && $this->urgente != -1) {
             $this->filtros[] = ["urgente", "=", $this->urgente];
         }
 
-        if($this->finalizada !== null && $this->finalizada != -1){
+        if ($this->finalizada !== null && $this->finalizada != -1) {
             $this->filtros[] = ["finalizada", "=", $this->finalizada];
         }
 
-        if($this->estimativa){
+        if ($this->estimativa) {
             $this->filtros[] = ["estimativa", "=", $this->estimativa];
         }
 
         $this->filtros[] = ["setor", "=", $this->setor];
     }
 
-    public function limpaFiltros(){
+    public function limpaFiltros()
+    {
         $this->filtros = null;
         $this->descricao = null;
         $this->solicitante_id = null;
@@ -95,10 +102,10 @@ class Pagina extends Component
     public function render()
     {
         // dd($this->filtros);
-        if($this->filtros){
-            $demandas = Demanda::where($this->filtros)->orderBy("created_at", "DESC")->get();
-        }else{
-            $demandas = Demanda::where("setor", $this->setor)->orderBy("created_at", "DESC")->get();
+        if ($this->filtros) {
+            $demandas = Demanda::where($this->filtros)->orderBy("estimativa", "DESC")->get();
+        } else {
+            $demandas = Demanda::where("setor", $this->setor)->where('finalizada', 0)->orderBy("estimativa", "DESC")->get();
         }
         return view('livewire.demandas.consultar.pagina', ["demandas" => $demandas]);
     }
