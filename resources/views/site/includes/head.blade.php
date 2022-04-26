42f40ccdbbf20ecc35228f9a9c2b734d0f536b96
evvvrado <everaldocrj@gmail.com>
2022-04-26 15:32:45 -0300

    SAVEPOINT

--- resources/views/site/includes/head.blade.php
+++ resources/views/site/includes/head.blade.php
@@ -2,58 +2,74 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta name="description" content="Congresso e Feira Fitness. Cursos, Palestras e Workshops de atualização profissional" />
-  <meta name="keywords" content="Congresso, Feira, Fitness, Cursos, Palestras, Workshops" />
-  <meta name="author" content="7Seven Trends" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description"
+        content="Congresso e Feira Fitness. Cursos, Palestras e Workshops de atualização profissional" />
+    <meta name="keywords" content="Congresso, Feira, Fitness, Cursos, Palestras, Workshops" />
+    <meta name="author" content="7Seven Trends" />
 
-  <meta property="og:title" content="ENAF, - Esporte, Fitness e Saúde" />
-  <meta property="og:url" content="enafdigital.com.br" />
-  <meta property="og:description" content="Congresso e Feira Fitness. Cursos, Palestras e Workshops de atualização profissional" />
-  <meta property="og:image" content="{{ asset('site/img/_og140.png') }} " />
+    <meta property="og:title" content="ENAF, - Esporte, Fitness e Saúde" />
+    <meta property="og:url" content="enafdigital.com.br" />
+    <meta property="og:description"
+        content="Congresso e Feira Fitness. Cursos, Palestras e Workshops de atualização profissional" />
+    <meta property="og:image" content="{{ asset('site/img/_og140.png') }} " />
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 
-  <link rel="stylesheet" href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css" />
+    <link rel="stylesheet"
+        href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css" />
 
-  <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" />
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" />
 
-  <link rel="preload" href="{{ asset('/site/css/style.css') }}" as="style" />
-  <link rel="preload" href="{{ asset('/site/js/jquery.min.js') }}" as="script" />
-  <link rel="preload" href="{{ asset('/site/js/jquery-ui.js') }}" as="script" />
-  <link rel="preload" href="{{ asset('/site/js/main.js') }}" as="script" />
-  <link rel="stylesheet" href="{{ asset('/site/css/style.css') }}" />
+    <link rel="preload" href="{{ asset('/site/css/style.css') }}?v=2" as="style" />
+    <link rel="preload" href="{{ asset('/site/js/jquery.min.js') }}?v=2" as="script" />
+    <link rel="preload" href="{{ asset('/site/js/jquery-ui.js') }}?v=2" as="script" />
+    <link rel="preload" href="{{ asset('/site/js/main.js') }}" as="script" />
+    <link rel="stylesheet" href="{{ asset('/site/css/style.css') }}?v=2" />
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-NFRK1GXJXY"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NFRK1GXJXY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'G-NFRK1GXJXY');
-  </script>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-  <meta name="facebook-domain-verification" content="g43nelr9ia3gs6rctulsoh1epvbrkx" />
+        gtag('config', 'G-NFRK1GXJXY');
+    </script>
 
-  <!-- Meta Pixel Code -->
-  <script>
-    !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '694950568550116');
-  fbq('track', 'PageView');
-  
-  </script>
-  <noscript>
-    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=694950568550116&ev=PageView&noscript=1" />
-  </noscript>
+    <meta name="facebook-domain-verification" content="g43nelr9ia3gs6rctulsoh1epvbrkx" />
+
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '694950568550116');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=694950568550116&ev=PageView&noscript=1" />
+    </noscript>
 
 </head>
