724ea2b28842e615b7baeed0a3b64f856fc3f61b
Luis Gustavo <gusouza980@gmail.com>
2022-07-01 14:48:27 -0300

    Update

--- app/Http/Controllers/PagamentosController.php
+++ app/Http/Controllers/PagamentosController.php
@@ -14,6 +14,33 @@ class PagamentosController extends Controller
         return view("painel.pagamentos.vendas", ["vendas" => $vendas]);
     }
 
+    public function cancelar(){
+        if($venda->forma == 0){
+            $venda->boleto->status = "canceled";
+            $venda->save();
+        }elseif($venda->forma == 1){
+            if($venda->gateway == 1){
+                $venda->cartao->status = 2;
+                $venda->save();
+            }else{
+                if ($venda->cartao){
+                    $venda->cartao->status = 6;
+                    $venda->save();
+                }
+            }
+        }else{
+            if($venda->carne && $venda->carne->parcelas > 0){
+                foreach($venda->carne->parcelas as $parcela){
+                    $parcela->status = "canceled";
+                    $parcela->save();
+                }
+            }   
+        }
+
+        toastr()->success("Pagamento marcado como cancelado!");
+        return redirect()->back();
+    }
+
 
     public function mapa()
     {
